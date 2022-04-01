2ef0eb6bf109c28990feca6d895fcd26c1e2637f
Sean Latham <ipimusicdev@gmail.com>
2022-04-01 14:13:12 +0100

    Piece types now selected via tags

--- generator.js
+++ generator.js
@@ -3,99 +3,6 @@
 let gTrackPiecesImage = new Image();
 gTrackPiecesImage.src = "images/trackmaniaPieces.png";
 
-let gTrackPieceTypes = [
-{	//Straight
-	imageOffset: new Vector2D(0, 0),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-},
-{	//Sharp right
-	imageOffset: new Vector2D(32, 0),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(1, 0),
-	exitAngle: Math.PI * 0.5,
-},
-{	//Sharp left
-	imageOffset: new Vector2D(64, 0),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(-1, 0),
-	exitAngle: Math.PI * -0.5,
-},
-{	//Wide right
-	imageOffset: new Vector2D(96, 0),
-	imageDimensions: new Vector2D(64, 64),
-	renderOffset: new Vector2D(16, -16),
-	exitOffset: new Vector2D(2, -1),
-	exitAngle: Math.PI * 0.5,
-	collisionOffset: new Vector2D(0.5, -0.5),
-	collisionExtents: new Vector2D(1, 1),
-},
-{	//Wide left
-	imageOffset: new Vector2D(160, 0),
-	imageDimensions: new Vector2D(64, 64),
-	renderOffset: new Vector2D(-16, -16),
-	exitOffset: new Vector2D(-2, -1),
-	exitAngle: Math.PI * -0.5,
-	collisionOffset: new Vector2D(-0.5, -0.5),
-	collisionExtents: new Vector2D(1, 1),
-},
-{	//Snake left
-	imageOffset: new Vector2D(96, 64),
-	imageDimensions: new Vector2D(64, 64),
-	renderOffset: new Vector2D(-16, -16),
-	exitOffset: new Vector2D(-1, -2),
-	exitAngle: 0,
-	collisionOffset: new Vector2D(-0.5, -0.5),
-	collisionExtents: new Vector2D(1, 1),
-},
-{	//Snake right
-	imageOffset: new Vector2D(160, 64),
-	imageDimensions: new Vector2D(64, 64),
-	renderOffset: new Vector2D(16, -16),
-	exitOffset: new Vector2D(1, -2),
-	exitAngle: 0,
-	collisionOffset: new Vector2D(0.5, -0.5),
-	collisionExtents: new Vector2D(1, 1),
-},
-{	//Turbo
-	imageOffset: new Vector2D(0, 64),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-},
-{	//Super turbo
-	imageOffset: new Vector2D(32, 64),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-},
-];
-
-let gStartLinePieceType =
-{
-	imageOffset: new Vector2D(0, 32),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-};
-
-let gCheckpointPieceType =
-{
-	imageOffset: new Vector2D(32, 32),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-};
-
-let gFinishLinePieceType =
-{
-	imageOffset: new Vector2D(64, 32),
-	imageDimensions: new Vector2D(32, 32),
-	exitOffset: new Vector2D(0, -1),
-	exitAngle: 0,
-};
-
 let gPlacedPieces = [];
 let gRandom = null;
 let gZoomScale = 1;
@@ -202,7 +109,7 @@ let CanPlacePiece = function(translation, rotation, trackPieceType)
 {
 	let offsetPositions = ApplyPieceOffset(translation, rotation, trackPieceType);
 	return !DoesPieceCollide(translation, rotation, trackPieceType) && !IsOutOfBounds(translation) &&
-		!DoesPieceCollide(offsetPositions.translation, offsetPositions.rotation, gFinishLinePieceType) && !IsOutOfBounds(offsetPositions.translation);
+		!DoesPieceCollide(offsetPositions.translation, offsetPositions.rotation, gPieceTypes.roadFlatStraight) && !IsOutOfBounds(offsetPositions.translation);
 }
 
 let GenerateTrack = function(length, checkpointCount, seed)
@@ -210,10 +117,14 @@ let GenerateTrack = function(length, checkpointCount, seed)
 	gPlacedPieces.length = 0;
 	gRandom = seed ? mulberry32(seed) : mulberry32(Math.floor(Math.random() * 4294967296));
 
+	//Setup the tags that will be used to place pieces.
+	let pieceTagWhitelist = [ "road", "flat" ];
+
+	//Set starting position and create the start line.
 	let currentTranslation = new Vector2D(Math.floor(gRandom() * 24) - 12, Math.floor(gRandom() * 24) - 12);
 	let currentRotation = 0;
 	
-	PlacePiece(currentTranslation, currentRotation, gStartLinePieceType);
+	PlacePiece(currentTranslation, currentRotation, SelectSuitablePieceType(currentTranslation, currentRotation, pieceTagWhitelist.concat(["startLine"]), [], []));
 	currentTranslation.y -= 1;
 
 	let nextCheckpointIndex = checkpointCount > 0 ? length / (checkpointCount + 1) : -1;
@@ -228,9 +139,10 @@ let GenerateTrack = function(length, checkpointCount, seed)
 		//See if we need to place a checkpoint.
 		if (nextCheckpointIndex >= 0 && i >= Math.floor(nextCheckpointIndex))
 		{
-			if (CanPlacePiece(currentTranslation, currentRotation, gCheckpointPieceType))
+			let checkpointPieceType = SelectSuitablePieceType(currentTranslation, currentRotation, pieceTagWhitelist.concat(["checkpoint"]), [], []);
+			if (checkpointPieceType)
 			{
-				nextPieceType = gCheckpointPieceType;
+				nextPieceType = checkpointPieceType;
 				nextCheckpointIndex += length / (checkpointCount + 1);
 			}
 		}
@@ -238,21 +150,7 @@ let GenerateTrack = function(length, checkpointCount, seed)
 		//Randomly select a piece.
 		if (nextPieceType === null)
 		{
-			let possiblePieceTypes = [];
-			
-			for (let j = 0; j < gTrackPieceTypes.length; ++j)
-			{
-				let trackPieceType = gTrackPieceTypes[j];
-				if (lastDeadEndPieceType !== trackPieceType && CanPlacePiece(currentTranslation, currentRotation, trackPieceType))
-				{
-					possiblePieceTypes.push(gTrackPieceTypes[j]);
-				}
-			}
-			
-			if (possiblePieceTypes.length > 0)
-			{
-				nextPieceType = possiblePieceTypes[Math.floor(gRandom() * possiblePieceTypes.length)];
-			}
+			nextPieceType = SelectSuitablePieceType(currentTranslation, currentRotation, pieceTagWhitelist, [ "startLine", "checkpoint", "finishLine" ], [ lastDeadEndPieceType ]);
 		}
 		
 		//Place the piece (if we have one).
@@ -286,7 +184,7 @@ let GenerateTrack = function(length, checkpointCount, seed)
 		}
 	}
 	
-	PlacePiece(currentTranslation, currentRotation, gFinishLinePieceType); //Finish line
+	PlacePiece(currentTranslation, currentRotation, SelectSuitablePieceType(currentTranslation, currentRotation, pieceTagWhitelist.concat(["finishLine"]), [], []));
 }
 
 let RenderTrack = function(ctx)
@@ -305,10 +203,14 @@ let RenderTrack = function(ctx)
 		
 		ctx.translate(placedPiece.translation.x * 32, placedPiece.translation.y * 32);
 		ctx.rotate(placedPiece.rotation);
-		//ctx.scale(placedPiece.scale.x, placedPiece.scale.y);
 		
-		let renderOffset = placedPiece.trackPieceType.renderOffset || Vector2DStatic.CreateZeroVector();
-		
+		let renderOffset = Vector2DStatic.CreateZeroVector();
+		if (placedPiece.trackPieceType.collisionOffset)
+		{
+			renderOffset.x = placedPiece.trackPieceType.collisionOffset.x * 32;
+			renderOffset.y = placedPiece.trackPieceType.collisionOffset.y * 32;
+		}
+
 		ctx.drawImage(gTrackPiecesImage,
 			placedPiece.trackPieceType.imageOffset.x, placedPiece.trackPieceType.imageOffset.y, placedPiece.trackPieceType.imageDimensions.x, placedPiece.trackPieceType.imageDimensions.y,
 			(placedPiece.trackPieceType.imageDimensions.x * -0.5) + renderOffset.x, (placedPiece.trackPieceType.imageDimensions.y * -0.5) + renderOffset.y,
---
+++ piecetypes.js
@@ -0,0 +1,149 @@
+"use strict"
+
+//Piece types object.
+let gPieceTypes =
+{
+	roadFlatStraight:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(0, 0),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+	roadFlatCornerRight:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(32, 0),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(1, 0),
+		exitAngle: Math.PI * 0.5,
+	},
+	roadFlatCornerLeft:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(64, 0),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(-1, 0),
+		exitAngle: Math.PI * -0.5,
+	},
+	roadStartLine:
+	{
+		tags: [ "road", "flat", "startLine" ],
+		imageOffset: new Vector2D(0, 32),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+	roadFlatCheckpoint:
+	{
+		tags: [ "road", "flat", "checkpoint" ],
+		imageOffset: new Vector2D(32, 32),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+	roadFinishLine:
+	{
+		tags: [ "road", "flat", "finishLine" ],
+		imageOffset: new Vector2D(64, 32),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+	roadFlatCurveRight:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(96, 0),
+		imageDimensions: new Vector2D(64, 64),
+		exitOffset: new Vector2D(2, -1),
+		exitAngle: Math.PI * 0.5,
+		collisionOffset: new Vector2D(0.5, -0.5),
+		collisionExtents: new Vector2D(1, 1),
+	},
+	roadFlatCurveLeft:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(160, 0),
+		imageDimensions: new Vector2D(64, 64),
+		exitOffset: new Vector2D(-2, -1),
+		exitAngle: Math.PI * -0.5,
+		collisionOffset: new Vector2D(-0.5, -0.5),
+		collisionExtents: new Vector2D(1, 1),
+	},
+	roadFlatSBendLeft:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(96, 64),
+		imageDimensions: new Vector2D(64, 64),
+		exitOffset: new Vector2D(-1, -2),
+		exitAngle: 0,
+		collisionOffset: new Vector2D(-0.5, -0.5),
+		collisionExtents: new Vector2D(1, 1),
+	},
+	roadFlatSBendRight:
+	{
+		tags: [ "road", "flat" ],
+		imageOffset: new Vector2D(160, 64),
+		imageDimensions: new Vector2D(64, 64),
+		exitOffset: new Vector2D(1, -2),
+		exitAngle: 0,
+		collisionOffset: new Vector2D(0.5, -0.5),
+		collisionExtents: new Vector2D(1, 1),
+	},
+	roadFlatTurbo:
+	{
+		tags: [ "road", "flat", "engineBlock" ],
+		imageOffset: new Vector2D(0, 64),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+	roadFlatSuperTurbo:
+	{
+		tags: [ "road", "flat", "engineBlock" ],
+		imageOffset: new Vector2D(32, 64),
+		imageDimensions: new Vector2D(32, 32),
+		exitOffset: new Vector2D(0, -1),
+		exitAngle: 0,
+	},
+};
+
+let FindAllSuitablePieceTypes = function(translation, rotation, tagWhitelist, tagBlacklist, pieceTypeBlacklist)
+{
+	let suitablePieceTypes = [];
+	
+	Object.getOwnPropertyNames(gPieceTypes).forEach(pieceTypeKey =>
+	{
+		let pieceType = gPieceTypes[pieceTypeKey];
+		
+		//Is the piece type in the blacklist?
+		if (pieceTypeBlacklist.find(type => pieceType == type))
+			return;
+		
+		if (pieceType.tags)
+		{
+			//Are any of the tags in the blacklist?
+			if (tagBlacklist.find(tag => pieceType.tags.includes(tag)))
+				return;
+			
+			//Are all of the tags in the whitelist?
+			if (tagWhitelist.reduce((count, tag) => pieceType.tags.includes(tag) ? count + 1 : count, 0) != tagWhitelist.length)
+				return;
+		}
+		
+		//Check if the piece fits.
+		if (!CanPlacePiece(translation, rotation, pieceType))
+			return;
+		
+		suitablePieceTypes.push(pieceType);
+	});
+	
+	return suitablePieceTypes;
+}
+
+let SelectSuitablePieceType = function(translation, rotation, tagWhitelist, tagBlacklist, pieceTypeBlacklist)
+{
+	let allSuitablePieceTypes = FindAllSuitablePieceTypes(translation, rotation, tagWhitelist, tagBlacklist, pieceTypeBlacklist);
+	return allSuitablePieceTypes.length > 0 ? allSuitablePieceTypes[Math.floor(gRandom() * allSuitablePieceTypes.length)] : null;
+}
