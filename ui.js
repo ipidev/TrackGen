e9babae5f74252513c271ff6e163196868e3b816
Sean Latham <ipimusicdev@gmail.com>
2022-04-02 17:04:50 +0100

    Pieces not on the chosen view height are translucent

--- ui.js
+++ ui.js
@@ -3,26 +3,36 @@
 let gUI =
 {
 	zoomScale: 1,
+	viewLayer: 0,
 	trackPiecesImage: new Image(),
 };
 gUI.trackPiecesImage.src = "images/trackmaniaPieces.png";
 
-let RenderAll = function(ctx)
+let RenderAll = function(ctx, viewZ)
 {
-	RenderGrass(ctx);
-	RenderTrack(ctx);
+	RenderGrass(ctx, viewZ);
+	RenderTrack(ctx, viewZ);
 }
 
-let RenderGrass = function(ctx)
+let RenderGrass = function(ctx, viewZ)
 {
 	let priorCtxTransform = ctx.getTransform();
 
 	ctx.setTransform(new DOMMatrix());
-	ctx.fillStyle = "#247A27";
+
+	//Change grass to sky as viewing height increases - cute effect!
+	let grassToSkyAlpha = viewZ / 32;
+	let grassR = Lerp(36, 85, grassToSkyAlpha);
+	let grassG = Lerp(122, 160, grassToSkyAlpha);
+	let grassB = Lerp(39, 155, grassToSkyAlpha);
+	ctx.fillStyle = "rgb(" + grassR + "," + grassG + "," + grassB + ")";
+
+	ctx.globalAlpha = 1;
 	ctx.fillRect(0, 0, gCanvas.width, gCanvas.height);
 
 	ctx.setTransform(priorCtxTransform);
 
+	ctx.globalAlpha = 1;
 	ctx.fillStyle = "#FFFFFF";
 	ctx.globalAlpha = 0.075;
 
@@ -37,13 +47,22 @@ let RenderGrass = function(ctx)
 	ctx.globalAlpha = 1;
 }
 
-let RenderTrack = function(ctx)
+let RenderTrack = function(ctx, viewZ)
 {
 	let priorCtxTransform = ctx.getTransform();
 
-	for (let i = 0; i < gPlacedPieces.length; ++i)
+	let sortedPieces = gPlacedPieces.slice();
+	sortedPieces.sort((a, b) =>
 	{
-		let placedPiece = gPlacedPieces[i];
+		if (a.translation.z !== b.translation.z)
+			return a.translation.z > b.translation.z ? 1 : -1;
+		return 0;
+	});
+
+	for (let i = 0; i < sortedPieces.length; ++i)
+	{
+		//Determine piece position.
+		let placedPiece = sortedPieces[i];
 		let placedPieceType = placedPiece.trackPieceType;
 		
 		ctx.translate(placedPiece.translation.x * 32, placedPiece.translation.y * 32);
@@ -56,14 +75,43 @@ let RenderTrack = function(ctx)
 			renderOffset.y = placedPieceType.collisionOffset.y * 32;
 		}
 
-		ctx.drawImage(gUI.trackPiecesImage,
-			placedPieceType.imageOffset.x, placedPieceType.imageOffset.y,
-			placedPieceType.imageDimensions.x, placedPieceType.imageDimensions.y,
-			(placedPieceType.imageDimensions.x * -0.5) + renderOffset.x, (placedPieceType.imageDimensions.y * -0.5) + renderOffset.y,
-			placedPieceType.imageDimensions.x, placedPieceType.imageDimensions.y);
+		//Determine opacity based on the currently viewed height layer.
+		let compositionPasses = [ "source-over" ];
+		let alphaPasses = [];
+
+		let deltaZ = Math.floor(Math.abs(placedPiece.translation.z - viewZ));
+		switch (deltaZ)
+		{
+			case 0:		alphaPasses.push(1);	break;
+			case 1:		alphaPasses.push(0.4);	break;
+			case 2:		alphaPasses.push(0.3);	break;
+			default:	alphaPasses.push(0.15);	break;
+		}
+
+		//Darken or lighten the image if it's below or above the current layer.
+		if (deltaZ != 0)
+		{
+			compositionPasses.push(placedPiece.translation.z - viewZ > 0 ? "screen" : "multiply");
+			alphaPasses.push((1 - alphaPasses[0]) * 0.15);
+		}
+
+		for (let j = 0; j < alphaPasses.length; ++j)
+		{
+			ctx.globalAlpha = alphaPasses[j];
+			ctx.globalCompositeOperation = compositionPasses[j];
+
+			ctx.drawImage(gUI.trackPiecesImage,
+				placedPieceType.imageOffset.x, placedPieceType.imageOffset.y,
+				placedPieceType.imageDimensions.x, placedPieceType.imageDimensions.y,
+				(placedPieceType.imageDimensions.x * -0.5) + renderOffset.x, (placedPieceType.imageDimensions.y * -0.5) + renderOffset.y,
+				placedPieceType.imageDimensions.x, placedPieceType.imageDimensions.y);
+		}
 		
 		ctx.setTransform(priorCtxTransform);
 	}
+
+	ctx.globalAlpha = 1;
+	ctx.globalCompositeOperation = "source-over";
 }
 
 let SetCanvasZoom = function(scale)
@@ -84,10 +132,32 @@ let SetCanvasZoom = function(scale)
 // DOM events
 ////////////////////////////////////////////////////////////////////////////////
 
+let ClampNumberInput = function(e)
+{
+	if (e.target && e.isTrusted)
+	{
+		let value = e.target.valueAsNumber;
+
+		if (Number.isNaN(value))
+		{
+			e.target.value = value = 0;
+		}
+
+		if (e.target.min !== undefined && value < parseFloat(e.target.min))
+		{
+			e.target.value = e.target.min;
+		}
+		else if (e.target.max !== undefined && value > parseFloat(e.target.max))
+		{
+			e.target.value = e.target.max;
+		}
+	}
+}
+
 let OnGenerateButtonPressed = function()
 {
-	let trackLength = parseFloat(document.getElementById("trackLength").value);
-	let trackCheckpoints = parseFloat(document.getElementById("trackCheckpoints").value);
+	let trackLength = document.getElementById("trackLength").valueAsNumber;
+	let trackCheckpoints = document.getElementById("trackCheckpoints").valueAsNumber;
 	let trackSeed = document.getElementById("trackSeed").value ? crc32(document.getElementById("trackSeed").value) : null;
 	
 	let trackMaterialWhitelist = [];
@@ -102,7 +172,31 @@ let OnGenerateButtonPressed = function()
 	}
 
 	GenerateTrack(trackLength, trackCheckpoints, trackSeed, trackMaterialWhitelist);
-	RenderAll(gCtx);
+
+	//Determine new view layer limits from generated track.
+	let newViewZ = 0;
+	if (gPlacedPieces.length > 0)
+	{
+		newViewZ = gPlacedPieces[0].translation.z;
+		let minimumZ = newViewZ;
+		let maximumZ = newViewZ;
+
+		gPlacedPieces.forEach(placedPiece =>
+		{
+			if (minimumZ > placedPiece.translation.z)
+				minimumZ = placedPiece.translation.z;
+			
+			if (maximumZ < placedPiece.translation.z)
+				maximumZ = placedPiece.translation.z;
+		});
+
+		let trackViewLayerElement = document.getElementById("trackViewLayer");
+		trackViewLayerElement.min = Math.round(minimumZ);
+		trackViewLayerElement.max = Math.round(maximumZ);
+		trackViewLayerElement.value = Math.round(newViewZ);
+	}
+
+	RenderAll(gCtx, newViewZ);
 	
 	GenerateNewTitle();
 }
@@ -123,6 +217,18 @@ let OnZoomButtonPressed = function(e)
 	RenderAll(gCtx);
 }
 
+let OnViewLayerChanged = function(e)
+{
+	//Skip programmatic changes
+	if (!e.isTrusted)
+		return;
+
+	ClampNumberInput(e);
+
+	let newViewZ = parseFloat(e.target.value);
+	RenderAll(gCtx, newViewZ);
+}
+
 let OnPageLoaded = function(e)
 {
 	SetCanvasZoom(1);
@@ -130,6 +236,9 @@ let OnPageLoaded = function(e)
 
 	OnGenerateButtonPressed();
 	
+	document.getElementById("trackLength").addEventListener("input", ClampNumberInput);
+	document.getElementById("trackCheckpoints").addEventListener("input", ClampNumberInput);
 	document.getElementById("generateButton").addEventListener("click", OnGenerateButtonPressed);
 	document.getElementById("zoomButton").addEventListener("click", OnZoomButtonPressed);
+	document.getElementById("trackViewLayer").addEventListener("input", OnViewLayerChanged);
 };
